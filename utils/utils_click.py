d3d49efd433cdc647b25dc3e04fb7d3ec3e2bf5a
connor <brockcc1@cardiff.ac.uk>
2024-06-07 02:15:30 +0100

    trying to build upon welcome issues

--- utils/utils_click.py
+++ utils/utils_click.py
@@ -12,46 +12,72 @@ Furthermore, each creation function is only deployed once, therefore we
 may as well encalpuate it into one big group, rather then have them
 scattered and separately called in the main function.
 
+
 """
 
 console = Console()
 
 
 @click.group()
-def cli():
+def setup():
     pass
 
 
-@cli.command(help="Change your username.")
-@click.option("--username", prompt="Enter your name", help="The username to create")
-def create_user(username):
+@setup.command(name='c', help="Change your username.")
+def create_user():
+    username = click.prompt("Enter your name", type=str)
     json_file.create_mood_data(username, f"{username}_mood.json")
     console.print(f"[bold cyan]{username}[/bold cyan] has been created. [bold cyan]Welcome :)[/bold cyan]",
                   style="bold red")
     # Each user should have a specific file for them, rather than one nested variable.
     # Usernames being input into the to get around the issue of two parameters being required for the method.
 
-@cli.command(help="Input a score.")
-@click.option("--score", prompt="How do you feel on a scale of one to five?", type=int, help="Your score from 1 to 5")
-def obtain_score(score):
+
+@setup.command(name='s', help="Input a score.")
+def obtain_score():
+    score = click.prompt("how do you feel?", type=int)
     if score >= 5:
         console.print("[red]Damn, you feel great![/red]")
     else:
         console.print("[green]Keep going, you're doing well![/green]")
 
+# cli.add_command(obtain_score)
+# cli.add_command(create_user)
 
-@click.command()
-@click.argument('welcome', required=False)
-@click.option('--welcome', default=False)
-@click.pass_context
-def welcome_setup():
-    username = click.prompt("Enter your username")
-    create_user.invoke(
-        create_user(username)
-    )
-    score = click.prompt("How do you feel on a scale of one to five?", type=int)
-    obtain_score.invoke(obtain_score(score))
 
+# @cli.command(name='w', help='welcome page')
+# @click.argument('username')
+# @click.argument('score')
+# def welcome_setup(username, score):
+#     click.echo("Welcome page")
+#     click.echo("Enter username:")
+
+
+
+    """json_file.create_mood_data(username, f"{username}_mood.json")
+    click.echo(f"Welcome to mood! {welcome}")
+    create_user(username)
+    obtain_score(score)"""
+
+
+# @cli.command(name='w', help='welcome page')
+# @click.option('--welcome', default=False)
+# def welcome_setup(welcome):
+#     click.echo(welcome)
+#     username = input("enter user")
+#     create_user(username)
+#     score = input("enter score")
+#     obtain_score(score)
+
+"""
+Multiple issues :
+
+1) Welcome function does not work
+2) When grouped, the options don't show as options
+3) When only one function is called, it only lists that particular function as an option
+
+"""
 
 if __name__ == "__main__":
-    cli()
+    setup_commands = ['c', 's']
+    setup(args=setup_commands, prog_name='setup')
