3002f9108cc5cd7fead1fdb91adaa20c8c72687a
connor <brockcc1@cardiff.ac.uk>
2024-06-12 00:28:42 +0100

    Removed creation of file, too many issues for such little gain. Implemented welcome function, as well as a delete function.

--- utils/utils_click.py
+++ utils/utils_click.py
@@ -1,11 +1,12 @@
 import click
-import utils.utils_json as json_file
 from rich.console import Console
 from usr_info.user import logged_in_user
 import utils.utils_tinydb as td
 import datetime
+import os
 
 console = Console()
+dt_now = str(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
 
 
 @click.group()
@@ -13,38 +14,27 @@ def cli():
     pass
 
 
-@cli.command(name='c', help="Create a file using the current logged in username.")
-def create_user():
-    if td.initialize_db():
-        console.print(f"A file for [bold cyan]{logged_in_user}[/bold cyan] has been created. [bold cyan]Welcome :)[/bold cyan]", style="bold red")
-    else:
-        console.print(f"Oops, a file already exists for [bold cyan]{logged_in_user}[/bold cyan].")
+def welcome():
+    click.echo("Welcome to mood, a mood tracker to help you manage your mood (obviously).")
 
 
 @cli.command(name='s', help="Input a score.")
 def obtain_score():
-    score = click.prompt("How do you feel?", type=int)
-    dt_now = str(datetime.datetime.now())
+    score = int(click.prompt("How do you feel?", type=int))
     if score >= 5:
         console.print("[red]Damn, you feel great![/red]")
     else:
         console.print("[green]Keep going, you're doing well![/green]")
-    td.write_mood_date(dt_now, score)
+    td.write_mood_data(dt_now, score)
 
 
-"""
+@cli.command(name='d', help="Delete User File.")
+def delete_user():
+    user_input = click.prompt('Are you sure you want to delete this user?', type=click.Choice(['y', 'n']))
+    if user_input == 'y':
+        td.delete_mood_data()
+        os.remove(f"{logged_in_user}.txt") # Deleting the first-time startup check so user can start from scratch.
+        console.print(f"[red]Deleted file for {logged_in_user}[/red]")
 
-We don't execute the check for a first time user here, we do it in the utils_json.py file. From there, we can check 
-from a file-level (?) if the user exists, if not, "Welcome! :) Function should be stored inside main for now, 
-just to speed development up slightly.
-
-Why don't we just store the file upon first boot? using the os.getlogin method? We could avoid a "what's your name?"
-function to create the user and automatically set the file structure up using the name stored through the login. 
-The welcome message could just introduce the user to the program through echo statements, rather then ask them arbitrary 
-questions. Security issue though? Maybe transparency is key, e.g."A file has been created for you in your logged-in
-name, do you wish to change it?  
-
-
-"""
 
 
--- utils/utils_tinydb.py
+++ utils/utils_tinydb.py
@@ -1,26 +1,24 @@
+from functools import wraps
 from tinydb import TinyDB, Query
 from usr_info.user import logged_in_user
-from usr_info.user import user_template
 import datetime
 import os
 
 
-db = None
-mood_table = None
 query = Query()
 dt_now = str(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+db = TinyDB(f"{logged_in_user}_mood.json")
+mood_table = db.table('mood')
 
 
-def initialize_db():
-    global db, mood_table
-    if not os.path.exists(f"{logged_in_user}_mood.json"):
-        db = TinyDB(f"{logged_in_user}_mood.json")
-        mood_table = db.table('mood')
-        mood_table.insert({'File Created': dt_now})
-        return True
+def delete_mood_data():
+    if os.path.exists(f"{logged_in_user}_mood.json"):
+        os.remove(f"{logged_in_user}_mood.json")
 
 
+def write_mood_data(datetime_now, score):
+    mood_table.insert({'datetime': datetime_now, 'score': score})
 
-def write_mood_date(score):
-    mood_table.insert({'datetime': dt_now, 'score': score})
-
+# def retrieve_mood_date():
+#     initialize_db()
+#     db.search()
